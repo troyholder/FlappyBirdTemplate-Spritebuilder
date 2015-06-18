bb1152c515bab7549b1f8420b8c5107845c935aa
Troy Holder <gtholder@gmail.com>
2015-06-17 20:47:48 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -19,7 +19,7 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 {
     // define variables here;
     Character*      character;
-    CCPhysicsNode   physicsNode;
+    CCPhysicsNode*   physicsNode;
 }
 
 -(void) initialize;
